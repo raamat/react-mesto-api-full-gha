53bebac27494ebcd708b365c1dd656688f0a36e3
Raamat Volgograd <Gadjukino777@yandex.ru>
2023-09-21 09:42:17 +0300

    В файле app.js выставить порт по умолчанию 3000

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const helmet = require('helmet');
 const cors = require('cors');
 
-const { PORT = 4000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const { errors } = require('celebrate');
 const router = require('./routes/index');
 const handleErrors = require('./middlewares/handleErrors');
@@ -13,7 +13,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const app = express();
 app.use(express.json());
 
-/// app.use(cors({ origin: ['http://localhost:3000', 'https://mesto.raamat.pw'], credentials: true, maxAge: 30 }));
+// app.use(cors({ origin: ['http://localhost:3000', 'https://mesto.raamat.pw'], credentials: true, maxAge: 30 }));
 app.use(cors());
 
 app.use(helmet());
